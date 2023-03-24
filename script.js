5697fa656602422a12afda7fdac36c2bb3a45222
Yassine Ait Laaguidi <aitlaaguidiyassine@gmail.com>
2023-03-24 14:16:28 +0100

    first commit

--- script.js
+++ script.js
@@ -1,30 +1,112 @@
 'use strict';
 
-const modal = document.querySelector('.modal');
-const overlay = document.querySelector('.overlay');
-const btnCloseModal = document.querySelector('.close-modal');
-const btnsOpenModal = document.querySelectorAll('.show-modal');
+// Selecting elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
 
-const openModal = function () {
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+let scores, currentScore, activePlayer, playing;
+
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+//const scores = [0, 0];
+//let currentScore = 0;
+//let activePlayer = 0;
+//let playing = true;
+
+// Starting conditions
+const init = function () {
+    scores = [0, 0];
+    currentScore = 0;
+    activePlayer = 0;
+    playing = true;
+
+    score0El.textContent = 0;
+    score1El.textContent = 0;
+    current0El.textContent = 0;
+    current1El.textContent = 0;
+
+    diceEl.classList.add('hidden');
+    player0El.classList.remove('player--winner');
+    player1El.classList.remove('player--winner');
+    player0El.classList.add('player--active');
+    player1El.classList.remove('player--active');
+};
+init();
+
+const switchPlayer = function () {
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
 };
 
-const closeModal = function () {
-    modal.classList.add('hidden');
-    overlay.classList.add('hidden');
-};
+btnRoll.addEventListener('click', function(){
+   if(playing) {
+       // 1. Generating a random dice roll
+       const dice = Math.trunc(Math.random() * 6) + 1;
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-    btnsOpenModal[i].addEventListener('click', openModal);
+       // 2. Display dice
+       diceEl.classList.remove('hidden');
+       diceEl.src = `dice-${dice}.png`;
 
-btnCloseModal.addEventListener('click', closeModal);
-overlay.addEventListener('click', closeModal);
+       // 3. Check for rolled 1
+       if(dice !== 1) {
+           // Add dice to current score
+           currentScore += dice;
+           //current0El.textContent = currentScore;// change later
+           document.getElementById(
+               `current--${activePlayer}`
+           ).textContent = currentScore;
+       }else {
+           switchPlayer();
+       }
+   }
 
-document.addEventListener('keydown', function (e) {
-    // console.log(e.key);
-
-    if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
-        closeModal();
-    }
 });
+
+btnHold.addEventListener('click',function(e) {
+    if(playing) {
+        // 1. Add current score to active player's score
+        //scores at position player:
+        scores[activePlayer] += currentScore;
+        // scores[1] = scores[1] + currentScore
+
+        document.getElementById(`score--${activePlayer}`).textContent =
+            scores[activePlayer];
+
+
+        // 2. Check if player's score is >= 100
+        if (scores[activePlayer] >= 20) {
+            // Finish the game
+            playing = false;
+            diceEl.classList.add('hidden');
+
+            document
+                .querySelector(`.player--${activePlayer}`)
+                .classList.add('player--winner');
+            document
+                .querySelector(`.player--${activePlayer}`)
+                .classList.remove('player--active');
+        } else {
+            // Switch to the next player
+            switchPlayer();
+        }
+    }
+})
+
+btnNew.addEventListener('click', init);
+
+
+
